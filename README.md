a9bc68be9bffb40de8363da1990f8f9ca0327237
w3dong <w3dong@gmail.com>
2022-11-20 16:48:41 -0800

    update E2E test & Unit test

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Lab 8 - Starter
+Weiwen Dong
+1) I would say 2 because it's important to check code before push. We need to do quality control before commit, especially when the recipe project is a little bit complex project that involves multiple js files. This would also help avoid situation that we find error in code later and do multiple resubmission to fix it, which provide spaces for github conflicts. 
+
+2) No.
+
+3) No. This feature involves multiple interaction (write & send & response) and it's too complex for a unit testing. 
+   
+4) Yes. This feature is a simple and in small scale that can be tested using simple if statements. Also, it does not affect other app features like write & send. 
+
+
+
+
+
+
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,72 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+// test phone number
+test('123-456-7890 is a valid phone number', () => {
+    expect(functions.isPhoneNumber("123-456-7890")).toBe(true);
+});
+test('934-456-7120 is a valid phone number', () => {
+    expect(functions.isPhoneNumber("934-456-7120")).toBe(true);
+});
+test('12 is not a valid phone number', () => {
+    expect(functions.isPhoneNumber("12")).toBe(false);
+});
+test('12380 is not a valid phone number', () => {
+    expect(functions.isPhoneNumber("12380")).toBe(false);
+});
+
+// test email
+test('w3@ucsd.edu is a valid email', () => {
+    expect(functions.isEmail("w3@ucsd.edu")).toBe(true);
+});
+test('w3@gmail.com is a valid email', () => {
+    expect(functions.isEmail("w3@gmail.com")).toBe(true);
+});
+test('w3dong is not a valid email', () => {
+    expect(functions.isEmail("w3dong")).toBe(false);
+});
+test('leetcode@ is not a valid email', () => {
+    expect(functions.isEmail("leetcode@ ")).toBe(false);
+});
+
+// test strong password
+test('cat_love is a valid strong password', () => {
+    expect(functions.isStrongPassword("cat_is_good")).toBe(true);
+});
+test('coffee_lattee is a valid strong password', () => {
+    expect(functions.isStrongPassword("coffee_lattee")).toBe(true);
+});
+test('12345 is not a valid strong password', () => {
+    expect(functions.isStrongPassword("12345")).toBe(false);
+});
+test('ab is not a valid strong password', () => {
+    expect(functions.isStrongPassword("ab")).toBe(false);
+});
+
+// test date
+test('11/20/2022 is a valid date', () => {
+    expect(functions.isDate("11/20/2022")).toBe(true);
+});
+test('3/2/2002 is a valid date', () => {
+    expect(functions.isDate("3/2/2002")).toBe(true);
+});
+test('300/2/2002 is a invalid date', () => {
+    expect(functions.isDate("300/2/2002")).toBe(false);
+});
+test('3/2/200982 is a invalid date', () => {
+    expect(functions.isDate("3/2/200982")).toBe(false);
+});
+
+// test hex color
+test('FF5733 is a valid hex color', () => {
+    expect(functions.isHexColor("FF5733")).toBe(true);
+});
+test('FFFFFF is a valid hex color', () => {
+    expect(functions.isHexColor("FFFFFF")).toBe(true);
+});
+test('AB is a invalid hex color', () => {
+    expect(functions.isHexColor("AB")).toBe(false);
+});
+test('0 is a invalid hex color', () => {
+    expect(functions.isHexColor("0")).toBe(false);
+});
